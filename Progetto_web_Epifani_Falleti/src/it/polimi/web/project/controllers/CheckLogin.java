05848b7430ccddead79ddf682fd35cc240a95e9b
giuxlb <61745729+giuxlb@users.noreply.github.com>
2020-05-23 11:32:41 +0200

    Added CheckLogin.

---
+++ Progetto_web_Epifani_Falleti/src/it/polimi/web/project/controllers/CheckLogin.java
@@ -0,0 +1,82 @@
+package it.polimi.web.project.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringEscapeUtils;
+
+
+import it.polimi.web.project.beans.User;
+import it.polimi.web.project.utils.ConnectionHandler;
+import it.polimi.web.project.DAO.UserDao;
+
+@WebServlet("/CheckLogin")
+public class CheckLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection connection = null;
+
+	public CheckLogin() {
+		super();
+	}
+
+	public void init() throws ServletException {
+		connection = ConnectionHandler.getConnection(getServletContext());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// obtain and escape params
+		PrintWriter out = response.getWriter();
+		String usrn = null;
+		String pwd = null;
+		try {
+			usrn = StringEscapeUtils.escapeJava(request.getParameter("username"));
+			pwd = StringEscapeUtils.escapeJava(request.getParameter("pwd"));
+		} catch (NullPointerException e) {
+			// for debugging only e.printStackTrace();
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing param values");
+			return;
+		}
+
+		// query db to authenticate for user
+		UserDao userDao = new UserDao(connection);
+		User user = null;
+		try {
+			user = userDao.checkCredentials(usrn, pwd);
+		} catch (SQLException e) {
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Not Possible to check credentials");
+			return;
+		}
+
+		// If the user exists, add info to the session and go to home page, otherwise
+		// show login page with error message
+
+		String path;
+		if (user == null) {
+			response.setContentType("text/plain");
+			out.println("Non sei registrato");
+		} else {
+			request.getSession().setAttribute("user", user);
+			path = getServletContext().getContextPath() + "/Home";
+			response.sendRedirect(path);
+		}
+
+	}
+
+	public void destroy() {
+		try {
+			ConnectionHandler.closeConnection(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
