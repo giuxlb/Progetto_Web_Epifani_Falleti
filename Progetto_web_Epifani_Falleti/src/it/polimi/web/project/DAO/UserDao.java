0eaa7c1035eef6c8af1c2f517a222c8fe6e65ee0
adrianofalleti98 <10566971@polimi.it>
2020-05-23 10:45:05 +0200

    Added UserDao

--- Progetto_web_Epifani_Falleti/src/it/polimi/web/project/DAO/UserDao.java
+++ Progetto_web_Epifani_Falleti/src/it/polimi/web/project/DAO/UserDao.java
@@ -16,7 +16,7 @@ public class UserDao {
 	}
 
 	public User checkCredentials(String username, String password) throws SQLException {
-
+//preparo la query
 		String query = "SELECT userID,name,surname FROM user where username = ? and password = ?";
 		try (PreparedStatement pstatement = con.prepareStatement(query)) {
 			pstatement.setString(1, username);
