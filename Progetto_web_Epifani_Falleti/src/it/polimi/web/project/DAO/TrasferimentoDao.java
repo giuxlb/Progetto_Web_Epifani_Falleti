f0267aa9050b71b9cd3bbe3f7447abc38be6bce7
adrianofalleti98 <10566971@polimi.it>
2020-05-24 11:51:26 +0200

    Added ContoDao and TrasferimentoDao. Modified the getter's name for attribute saldo in Conto

---
+++ Progetto_web_Epifani_Falleti/src/it/polimi/web/project/DAO/TrasferimentoDao.java
@@ -0,0 +1,63 @@
+package it.polimi.web.project.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import it.polimi.web.project.beans.Conto;
+
+public class TrasferimentoDao {
+
+	private Connection con;
+
+	public TrasferimentoDao(Connection c) {
+		this.con = c;
+	}
+
+	public boolean createTrasferimento(int destUserID, int destContoID, int userID, int contoID, int importo,
+			String causale) throws SQLException {
+		ContoDao cdao = new ContoDao(con);
+
+		List<Conto> conti = cdao.findContoByUser(destUserID);
+
+		boolean verified = false;
+
+		for (int i = 0; i < conti.size(); i++) {
+			if (conti.get(i).getID() == destContoID) // abbiamo trovato il conto
+			{
+				if (conti.get(i).getSaldo() > importo)
+					verified = true;
+				break;
+			}
+		} // qui ho controllato se il conto sotto quel contoID è effettivamente dello user
+			// id e se il saldo di quel conto, permette un trasferimento di quell'importo
+
+		// se verified è ancora false, ritorniamo false
+		if (!verified)
+			return verified;
+		int importoDest = 0 - importo;
+		// qui verified sarà true, quindi possiamo modificare sia l'importo del conto di
+		// destinazione, sia quello di quello di origine origine e aggiungere il
+		// trasferimento al dbms
+		cdao.changeSaldo(importoDest, destContoID);
+		cdao.changeSaldo(importo, contoID);
+
+		java.util.Date d = new java.util.Date();
+
+		String query = "INSERT INTO esercizio4.trasferimento (DestContoID,ContoID,causale,importo,data) VALUES(?,?,?,?,?)";
+		try (PreparedStatement statement = con.prepareStatement(query)) {
+			statement.setInt(1, destContoID);
+			statement.setInt(2, contoID);
+			statement.setString(3, causale);
+			statement.setInt(4, importo);
+			statement.setDate(5, new java.sql.Date(d.getTime()));
+			statement.execute();
+
+		}
+
+		// TO-DO ricordare a peppe l'aggiunta del parametro session.contoID
+		return true;
+	}
+
+}
--- Progetto_web_Epifani_Falleti/src/it/polimi/web/project/beans/Conto.java
+++ Progetto_web_Epifani_Falleti/src/it/polimi/web/project/beans/Conto.java
@@ -1,31 +1,31 @@
 package it.polimi.web.project.beans;
 
 public class Conto {
-	
+
 	private int id;
 	private int saldo;
 	private int userID;
-	
-	public int getID(){
+
+	public int getID() {
 		return this.id;
 	}
-	
+
 	public void setID(int id) {
 		this.id = id;
 	}
-	
-	public int getImporto() {
+
+	public int getSaldo() {
 		return this.saldo;
 	}
-	
+
 	public void setSaldo(int saldo) {
 		this.saldo = saldo;
 	}
-	
-	public int getUserID(){
+
+	public int getUserID() {
 		return this.userID;
 	}
-	
+
 	public void setUserID(int userID) {
 		this.userID = userID;
 	}
