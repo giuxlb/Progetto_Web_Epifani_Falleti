b1594f1ec39ff96462f8dd1f77d8bb7c9e8d72f0
adrianofalleti98 <10566971@polimi.it>
2020-05-25 11:52:34 +0200

    Added GoToConfermaTrasferimento and Logout

--- Progetto_web_Epifani_Falleti/src/it/polimi/web/project/controllers/CreateTrasferimento.java
+++ Progetto_web_Epifani_Falleti/src/it/polimi/web/project/controllers/CreateTrasferimento.java
@@ -2,9 +2,7 @@ package it.polimi.web.project.controllers;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.SQLException;
-import java.text.ParseException;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -14,17 +12,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import java.text.SimpleDateFormat;
-
 import org.apache.commons.lang.StringEscapeUtils;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
-import it.polimi.web.project.utils.ConnectionHandler;
 import it.polimi.web.project.DAO.TrasferimentoDao;
 import it.polimi.web.project.beans.User;
+import it.polimi.web.project.utils.ConnectionHandler;
 
 /**
  * Servlet implementation class CreateTrasferimento
@@ -32,20 +28,19 @@ import it.polimi.web.project.beans.User;
 @WebServlet("/CreateTrasferimento")
 public class CreateTrasferimento extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Connection connection = null;
 	private TemplateEngine templateEngine;
 
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CreateTrasferimento() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public CreateTrasferimento() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
-    public void init() throws ServletException {
+	public void init() throws ServletException {
 		ServletContext servletContext = getServletContext();
 		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
 		templateResolver.setTemplateMode(TemplateMode.HTML);
@@ -56,17 +51,19 @@ public class CreateTrasferimento extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//Redirect the user to home if not logged in
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Redirect the user to home if not logged in
 		HttpSession session = request.getSession();
 		if (session.isNew() || session.getAttribute("user") == null) {
 			String loginpath = getServletContext().getContextPath() + "/index.html";
 			response.sendRedirect(loginpath);
 			return;
 		}
-		
+
 		boolean isRequestBad = false;
 		Integer DestUserID = null;
 		Integer DestContoID = null;
@@ -77,50 +74,52 @@ public class CreateTrasferimento extends HttpServlet {
 			DestContoID = Integer.parseInt(request.getParameter("destContoID"));
 			amount = Integer.parseInt(request.getParameter("importo"));
 			causale = StringEscapeUtils.escapeJava(request.getParameter("causale"));
-			isRequestBad = causale.isEmpty() || DestUserID < 0 || DestContoID <0 || amount <0;
-		}catch (NumberFormatException | NullPointerException e) {
+			isRequestBad = causale.isEmpty() || DestUserID < 0 || DestContoID < 0 || amount < 0;
+		} catch (NumberFormatException | NullPointerException e) {
 			isRequestBad = true;
 			e.printStackTrace();
 		}
-		if(isRequestBad) {
+		if (isRequestBad) {
 			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Incorrect or missing param values");
 			return;
 		}
-		
+
 		User user = (User) session.getAttribute("user");
 		TrasferimentoDao trasferimentoDao = new TrasferimentoDao(connection);
 		Integer UserID = user.getId();
 		Integer ContoID = (Integer) session.getAttribute("ContoID");
-		//Date data = System.currentTimeMillis();
+		// Date data = System.currentTimeMillis();
 		System.out.println("Sono qui");
 		int trasferimentoValue = -1;
 		try {
-			trasferimentoValue = trasferimentoDao.createTrasferimento(DestUserID, DestContoID, UserID, ContoID, amount, causale);
-		}catch(SQLException e) {
+			trasferimentoValue = trasferimentoDao.createTrasferimento(DestUserID, DestContoID, UserID, ContoID, amount,
+					causale);
+		} catch (SQLException e) {
 			e.printStackTrace();
 			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Bad request");
 		}
-		
+
 		ServletContext servletContext = getServletContext();
 		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
 		String path;
-		switch(trasferimentoValue) {
-		
-		case(0): ctx.setVariable("errorMsg", "The Conto ID you entered dodn't match with any User ID");
-		path = "/StatoConto.html";
-		templateEngine.process(path, ctx, response.getWriter());
-		break;
-		case(1): ctx.setVariable("errorMsg", "The amount you entered isn't available in your balance");
-		path = "/StatoConto.html";
-		templateEngine.process(path, ctx, response.getWriter());
-		break;
-		case(2):
-			path = getServletContext().getContextPath() + "/ConfermaTrasferimento.html";
+		switch (trasferimentoValue) {
+
+		case (0):
+			ctx.setVariable("errorMsg", "The User ID you entered doesn't own that bank account");
+			path = "/StatoConto.html";
+			templateEngine.process(path, ctx, response.getWriter());
+			break;
+		case (1):
+			ctx.setVariable("errorMsg", "The amount you entered isn't available in your balance");
+			path = "/StatoConto.html";
+			templateEngine.process(path, ctx, response.getWriter());
+			break;
+		case (2):
+			path = getServletContext().getContextPath() + "/GoToConfermaTrasferimento";
+			session.setAttribute("destContoID", DestContoID);
 			response.sendRedirect(path);
 		}
-		
-		
-		
+
 	}
 
 }
--- Progetto_web_Epifani_Falleti/src/it/polimi/web/project/controllers/GoToConfermaTrasferimento.java
+++ Progetto_web_Epifani_Falleti/src/it/polimi/web/project/controllers/GoToConfermaTrasferimento.java
@@ -1,11 +1,25 @@
 package it.polimi.web.project.controllers;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import it.polimi.web.project.DAO.ContoDao;
+import it.polimi.web.project.beans.Conto;
+import it.polimi.web.project.utils.ConnectionHandler;
 
 /**
  * Servlet implementation class GoToConfermaTrasferimento
@@ -13,27 +27,83 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/GoToConfermaTrasferimento")
 public class GoToConfermaTrasferimento extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public GoToConfermaTrasferimento() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private Connection connection = null;
+	private TemplateEngine templateEngine;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public GoToConfermaTrasferimento() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+
+		connection = ConnectionHandler.getConnection(getServletContext());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String indexPath = getServletContext().getContextPath() + "/index.html";
+		HttpSession session = request.getSession();
+		if (session.getAttribute("user") == null || session.isNew()) {
+			response.sendRedirect(indexPath);
+		}
+
+		Integer destcontoid = (Integer) session.getAttribute("destContoID");
+		Integer contoid = (Integer) session.getAttribute("ContoID");
+
+		ContoDao cdao = new ContoDao(connection);
+		Conto conto = new Conto();
+		try {
+			conto = cdao.findContoByContoID(destcontoid);
+		} catch (SQLException e) {
+			// for debugging only e.printStackTrace();
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Not possible to recover transfers");
+			return;
+		}
+
+		String path = "/ConfermaTrasferimento.html";
+		ServletContext servletContext = getServletContext();
+		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+		ctx.setVariable("saldoDest", conto.getSaldo());
+		ctx.setVariable("destContoID", conto.getID());
+		ctx.setVariable("destuserid", conto.getUserID());
+
+		try {
+			conto = cdao.findContoByContoID(contoid);
+		} catch (SQLException e) {
+			// for debugging only e.printStackTrace();
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Not possible to recover transfers");
+			return;
+		}
+
+		ctx.setVariable("saldo", conto.getSaldo());
+		ctx.setVariable("ContoID", conto.getID());
+		ctx.setVariable("userid", conto.getUserID());
+
+		templateEngine.process(path, ctx, response.getWriter());
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
